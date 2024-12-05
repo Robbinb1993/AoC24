46c86aa7da648197ba7e788307ebf057b3201e95
Robbin <robbin.borst@opticon.com>
2024-12-05 15:40:06 +0100

    Changed algorithm to topological sort.

--- day5/day5.cc
+++ day5/day5.cc
@@ -2,26 +2,55 @@
 using namespace std;
 
 int inOrder[100][100];
+vector<int> edges[100];
+int inDegree[100];
 
-bool order(vector<int>& V) {
-   bool valid = true;
-   for (int i = 0; i < int(V.size()) && valid; i++) {
-      for (int j = i + 1; j < int(V.size()); j++) {
-         if (inOrder[V[j]][V[i]]) {
-            swap(V[i], V[j]);
-            valid = false;
-            break;
+int topologicalSort(vector<int>& V) {
+   unordered_map<int, bool> inList;
+   memset(inDegree, 0, sizeof(inDegree));
+   for (auto& v : V) {
+      inList[v] = true;
+   }
+   for (auto& v : V) {
+      for (auto& u : edges[v]) {
+         inDegree[u]++;
+      }
+   }
+
+   vector<int> stk;
+   for (auto& v : V) {
+      if (inDegree[v] == 0) {
+         stk.push_back(v);
+      }
+   }
+
+   int middleIdx = int(V.size()) / 2;
+   int totSeen = 0;
+
+   while (!stk.empty()) {
+      int v = stk.back();
+      if (totSeen == middleIdx) {
+         return v;
+      }
+      totSeen++;
+      stk.pop_back();
+      for (auto& e : edges[v]) {
+         if (inList[e]) {
+            inDegree[e]--;
+            if (inDegree[e] == 0) {
+               stk.push_back(e);
+            }
          }
       }
    }
-   return valid;
+   return -1;
 }
 
 int main() {
    ios_base::sync_with_stdio(false);
    cin.tie(NULL);
 
-   freopen("aoc-2024-day-05-challenge-1.txt", "r", stdin);
+   freopen("aoc-2024-day-05-challenge-2.txt", "r", stdin);
    //get line until line is empty
    string line;
 
@@ -35,6 +64,7 @@ int main() {
       int v2 = stoi(right);
 
       inOrder[v1][v2] = 1;
+      edges[v1].push_back(v2);
    }
 
    int ans1 = 0, ans2 = 0;
@@ -57,8 +87,7 @@ int main() {
          }
       }
       if (!valid) {
-         while (!order(V));
-         ans2 += V[int(V.size()) / 2];
+         ans2 += topologicalSort(V);
       }
       else {
          ans1 += V[int(V.size()) / 2];
