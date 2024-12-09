a5d7c7b517a219bb43ce6223491f068e6973ad9a
Robbin Borst <robbin.borst@opticon.com>
2024-12-09 08:55:16 +0100

    Improved naming

--- day9/day9.cc
+++ day9/day9.cc
@@ -36,12 +36,12 @@ void upd(const int p, const int remSpace, const int L, const int R, const int id
    freeSpace[idx] = max(freeSpace[2 * idx], freeSpace[2 * idx + 1]);
 }
 
-pair<int, pair<int, int>> query(const int reqSpace, const int L, const int R, const int idx) {
+tuple<int, int, int> query(const int reqSpace, const int L, const int R, const int idx) {
    if (freeSpace[idx] < reqSpace) {
-      return {-1, {-1, -1}};
+      return {-1, -1, -1};
    }
    if (L == R) {
-      return {startPos[idx], {freeSpace[idx], L}};
+      return {startPos[idx], freeSpace[idx], L};
    }
    if (freeSpace[2 * idx] >= reqSpace) {
       return query(reqSpace, L, (L + R) / 2, 2 * idx);
@@ -105,23 +105,27 @@ int main() {
    idx = int(M.size()) - 1;
    while (idx >= 0) {
       if (M[idx] != -1) {
-         int ed = idx;
-         while (idx >= 0 && M[idx] == M[ed]) {
+         int fileEndIdx = idx;
+         while (idx >= 0 && M[idx] == M[fileEndIdx]) {
             idx--;
          }
-         int st = idx + 1;
+         int fileStartIdx = idx + 1;
 
-         int reqSpace = ed - st + 1;
+         int reqSpace = fileEndIdx - fileStartIdx + 1;
          auto res = query(reqSpace, 0, int(freeBlocks.size()) - 1, 1);
 
-         if (res.first != -1 && res.first < st) {
+         int blockStartPos = get<0>(res);
+         int blockSize = get<1>(res);
+         int blockIdx = get<2>(res);
+
+         if (blockStartPos != -1 && blockStartPos < fileStartIdx) {
             int itr = 0;
-            for (int i = st; i <= ed; i++) {
-               swap(M[i], M[res.first + itr]);
+            for (int i = fileStartIdx; i <= fileEndIdx; i++) {
+               swap(M[i], M[blockStartPos + itr]);
                itr++;
             }
-            int remSpace = res.second.first - reqSpace;
-            upd(res.second.second, remSpace, 0, int(freeBlocks.size()) - 1, 1);
+            int newRemainingSpace = blockSize - reqSpace;
+            upd(blockIdx, newRemainingSpace, 0, int(freeBlocks.size()) - 1, 1);
          }
       }
       else {
