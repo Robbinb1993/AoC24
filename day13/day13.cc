05ec2a8d7bbfbb04760c3893604fd183c890a384
Robbin <robbin.borst@opticon.com>
2024-12-13 20:21:39 +0100

    added dsa folder with DSU

--- day13/day13.cc
+++ day13/day13.cc
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include "../dsa/dsu.h"
 using namespace std;
 
 long long applyCramersRule(long long ax, long long ay, long long bx, long long by, long long px, long long py) {
@@ -23,9 +22,6 @@ long long applyCramersRule(long long ax, long long ay, long long bx, long long b
 int main() {
    ios_base::sync_with_stdio(false);
    cin.tie(NULL);
-
-   DSU dsu(1000000);
-
 
    freopen("in.txt", "r", stdin);
 
