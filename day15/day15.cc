1739c82af4e90173c77b5e9be08d3453dbc2c16e
Robbin <robbin.borst@opticon.com>
2024-12-15 18:09:38 +0100

    Combined part1 and part2

--- day15/day15.cc
+++ day15/day15.cc
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const bool IS_PART1 = false;
+bool IS_PART1 = true;
 
 int N, M;
 vector<string> grid;
 const int DX[4] = {-1, 0, 1, 0};
 const int DY[4] = {0, 1, 0, -1};
 unordered_map<char, int> directionMap = {{'^', 0}, {'>', 1}, {'v', 2}, {'<', 3}};
+vector<vector<int>> seen;
+int runItr = 0;
 
 bool isHorizontalMove(const int dir) {
    return dir == 1 || dir == 3;
@@ -38,6 +40,9 @@ pair<int, int> processSingleWidthMove(const int px, const int py, const int nx, 
 
 //Used to move the 1x2 blocks vertically.
 bool processMultiWidthMove(const int px, const int py, const int dir, const bool performMove) {
+   if (seen[px][py] == runItr) return true;
+   seen[px][py] = runItr;
+
    if (grid[px][py] == '#') return false;
    if (grid[px][py] == '.') return true;
 
@@ -54,7 +59,7 @@ bool processMultiWidthMove(const int px, const int py, const int dir, const bool
    bool isRightHalfProcessed = (grid[nx][nry] == ']');
    // Check feasibility for both parts (left and right) of current [] block to be moved.
    bool canMoveLeftHalf = processMultiWidthMove(nx, nly, dir, performMove);
-   bool canMoveRightHalf = !isRightHalfProcessed ? processMultiWidthMove(nx, nry, dir, performMove) : true;
+   bool canMoveRightHalf = processMultiWidthMove(nx, nry, dir, performMove);
 
    if (!canMoveLeftHalf || !canMoveRightHalf) return false;
 
@@ -77,71 +82,84 @@ pair<int, int> move(const int px, const int py, const int dir) {
       return processSingleWidthMove(px, py, nx, ny, dir);
    }
    else {
+      runItr++;
       if (processMultiWidthMove(nx, ny, dir, false)) {
+         runItr++;
          processMultiWidthMove(nx, ny, dir, true);
          grid[nx][ny] = '@';
          grid[px][py] = '.';
          return {nx, ny};
       }
-      return {px, py};
    }
+   return {px, py};
 }
 
 int main() {
    ios_base::sync_with_stdio(false);
    cin.tie(NULL);
 
-   freopen("in.txt", "r", stdin);
+   auto start = chrono::high_resolution_clock::now();
+
+   freopen("aoc-2024-day-15-challenge-4.txt", "r", stdin);
 
    string line;
    while (getline(cin, line) && line != "") {
-      string transformedLine = "";
-      for (auto c : line) {
-         if (!IS_PART1) {
-            if (c == '#') transformedLine += "##";
-            else if (c == '.') transformedLine += "..";
-            else if (c == 'O') transformedLine += "[]";
-            else transformedLine += "@.";
-         }
-         else {
-            transformedLine = line;
-         }
-      }
-      grid.push_back(transformedLine);
+      grid.push_back(line);
    }
+   auto gridCopy = grid;
 
    string moves;
    while (getline(cin, line)) {
       moves += line;
    }
 
-   N = grid.size();
-   M = grid[0].size();
-
-   pair<int, int> pos;
-   for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-         if (grid[i][j] == '@') {
-            pos.first = i;
-            pos.second = j;
-            break;
+   for (int part = 1; part <= 2; part++) {
+      if (part == 2) {
+         IS_PART1 = false;
+         for (int l = 0; l < int(gridCopy.size()); l++) {
+            string transformedLine = "";
+            for (auto c : gridCopy[l]) {
+               if (c == '#') transformedLine += "##";
+               else if (c == '.') transformedLine += "..";
+               else if (c == 'O') transformedLine += "[]";
+               else transformedLine += "@.";
+            }
+            grid[l] = transformedLine;
          }
       }
-   }
 
-   for (auto m : moves) {
-      pos = move(pos.first, pos.second, directionMap[m]);
-   }
+      N = grid.size();
+      M = grid[0].size();
+      seen.assign(N, vector<int>(M, 0));
 
-   long long ans = 0;
-
-   for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-         if (grid[i][j] == '[' || grid[i][j] == 'O') ans += 100 * i + j;
+      pair<int, int> pos;
+      for (int i = 0; i < N; i++) {
+         for (int j = 0; j < M; j++) {
+            if (grid[i][j] == '@') {
+               pos.first = i;
+               pos.second = j;
+               break;
+            }
+         }
       }
+
+      for (auto m : moves) {
+         pos = move(pos.first, pos.second, directionMap[m]);
+      }
+
+      long long ans = 0;
+
+      for (int i = 0; i < N; i++) {
+         for (int j = 0; j < M; j++) {
+            if (grid[i][j] == '[' || grid[i][j] == 'O') ans += 100 * i + j;
+         }
+      }
+
+      cout << ans << endl;
    }
 
-   cout << ans << endl;
+   auto stop = chrono::high_resolution_clock::now();
+   cout << "Time: " << chrono::duration_cast<chrono::milliseconds>(stop - start).count() << "ms" << endl;
 
    return 0;
 }
